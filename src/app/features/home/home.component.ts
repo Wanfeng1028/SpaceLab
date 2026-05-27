d43192005211916217d7677e14e7add498f56086
Wanfeng1028 <dingyu_1028@163.com>
2026-05-27 19:48:33 +0800

    auto: 自动提交 - 2 个文件更改 (2026-05-27 19:48:33)

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -15,7 +15,7 @@ import { HeroLightFieldScene } from '../../three/scenes/hero-particles.scene';
 import { HoloIcoScene } from '../../three/scenes/holo-ico.scene';
 import { GlobeOrbitScene } from '../../three/scenes/globe-orbit.scene';
 import { LaunchTelemetryOverlayComponent } from './components/launch-telemetry-overlay/launch-telemetry-overlay.component';
-import { SuccessCardComponent } from '../../shared/components/success-card/success-card.component';
+import { SpaceGlassModalComponent } from '../../shared/components/glass/modal/space-glass-modal.component';
 import { EarthObservatorySection } from './components/earth-observatory/earth-observatory.component';
 import { NeuralCoreSection } from './components/neural-core/neural-core.component';
 import { VisualSystemsSection } from './components/visual-systems/visual-systems.component';
@@ -30,7 +30,7 @@ import { CockpitDashboardSection } from './components/cockpit-dashboard/cockpit-
   imports: [
     ThreeCanvasComponent,
     LaunchTelemetryOverlayComponent,
-    SuccessCardComponent,
+    SpaceGlassModalComponent,
     EarthObservatorySection,
     NeuralCoreSection,
     VisualSystemsSection,
